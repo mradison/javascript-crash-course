1d0e0d6b338cbe51fb9de175893bcfe1db16f007
madison reynolds <madison.reynolds@phoenix.edu>
2023-08-22 20:57:41 -0700

    added some JavaScript arrays

---
+++ main.js
@@ -0,0 +1,20 @@
+// Arrays - variables that hold multiple values
+
+const fruits = ['apples', 'oranges', 'pears'];
+
+fruits[3] = 'grapes';
+
+fruits.push('mangos');
+
+fruits.unshift('strawberries');
+
+fruits.pop();
+
+console.log(Array.isArray('hello'));
+
+console.log(fruits.indexOf('oranges'));
+
+console.log(fruits);
+
+
+
