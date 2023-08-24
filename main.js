e7ae7f4eea36aee390e63ec1d1896580472b8b33
madison reynolds <madison.reynolds@phoenix.edu>
2023-08-23 22:58:43 -0700

    Added some Object Oriented Programming

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-const addNums = num1 => num1 + 5;
+//Constructor Function
+function Person (firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+}
 
-console.log(addNums(5));
+Person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear();
+}
 
-todos.forEach((todo) => console.log(todo));
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+}
+
+//Instantiate an object
+const person1 = new Person('John', 'Doe', '4-3-1980');
+const person2 = new Person('Mary', 'Doe', '4-15-1980');
+
+console.log(person2.getFullName());
+console.log(person1);
